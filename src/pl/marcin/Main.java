027520b476989cba1d28e3a1c0b078c8773b7f37
Marcin <gasiorek.mail@gmail.com>
2023-03-31 10:32:27 +0200

    implement muliticlient connections with server

--- src/pl/marcin/Main.java
+++ src/pl/marcin/Main.java
@@ -1,3 +1,5 @@
+package pl.marcin;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,16 +11,18 @@ public class Main {
     public static void main(String[] args) {
 
         try(ServerSocket serverSocket = new ServerSocket(5000)){
-            Socket socket = serverSocket.accept();
-            System.out.println("Client Connected");
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
             while(true){
+                Socket socket = serverSocket.accept();
+                System.out.println("Client Connected");
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
                 String echoString = input.readLine();
                 if(echoString.equals("exit")){
                     break;
                 }
                 output.println("Echo from server: " + echoString);
+                System.out.println(echoString);
             }
         }catch(IOException e){
             System.out.println("Server exception " + e.getMessage());
