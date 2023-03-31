72c3ae7479fb712bacbc9b7fa63287ce8b63059f
Marcin <gasiorek.mail@gmail.com>
2023-03-31 11:03:47 +0200

    multiThreaded server : each Thread for each connection.

--- src/pl/marcin/Main.java
+++ src/pl/marcin/Main.java
@@ -13,16 +13,29 @@ public class Main {
         try(ServerSocket serverSocket = new ServerSocket(5000)){
 
             while(true){
-                Socket socket = serverSocket.accept();
-                System.out.println("Client Connected");
-                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-                String echoString = input.readLine();
-                if(echoString.equals("exit")){
-                    break;
-                }
-                output.println("Echo from server: " + echoString);
-                System.out.println(echoString);
+                //code for multiuser managed by Threads:new Thread for every connection:
+                new Echoer(serverSocket.accept()).start();
+//                Another way of doing the same:
+//                Socket socket = serverSocket.accept();
+//                Echoer echoer = new Echoer(socket);
+//                echoer.start();
+
+                //code for one user only without Threads:
+//                Socket socket = serverSocket.accept();
+//                System.out.println("Client Connected");
+//                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+//                String echoString = input.readLine();
+//                try{
+//                    Thread.sleep(5000);
+//                }catch (InterruptedException e){
+//                    System.out.println("Thread interrupted");
+//                }
+//                if(echoString.equals("exit")){
+//                    break;
+//                }
+//                output.println("Echo from server: " + echoString);
+//                System.out.println(echoString);
             }
         }catch(IOException e){
             System.out.println("Server exception " + e.getMessage());
